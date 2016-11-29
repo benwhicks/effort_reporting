0f3bee7bbcb179f8a5e7d9f301ae8a2ef1e42e96
Ben Hicks <benwhicks@gmail.com>
2016-11-29 17:17:13 +1100

    big end Nov commit

--- effort.scraping.R
+++ effort.scraping.R
@@ -1,6 +1,31 @@
 datdir <- "/Users/benhicks/Documents/Data Analysis/Data-Oxley/Effort Reporting"
 efdatfile <- paste0(datdir, "/2016T4 Y12 effort data.xlsx")
 teacher.dat.folder <- paste0(datdir, "/2016T4 teacher data/")
+student.dat.file <- paste0(datdir,"/2016T4 Y12 student effort data.csv")
+teacher.dat.file <- paste0(datdir,"/2016T4 Y12 teacher effort data.csv")
+student.dat <- read.csv(student.dat.file)
+# the following needs to be merged and then outputed
+# teacher.dat <- read.csv(teacher.dat.file)
+
+effortMergeData <- function(studentData, teacherData) {
+  require(tidyr)
+  sdat <- studentData %>% gather("Category", "Score", 
+                                 c(Diligence, Engagement, Behaviour),
+                                 na.rm = TRUE)
+  sdat <- data.frame(sdat, Source = "Student")
+  tdat <- teacherData %>% gather("Category", "Score", 
+                                 c(Diligence, Engagement, Behaviour),
+                                 na.rm = TRUE)
+  tdat <- data.frame(tdat, Source = "Teacher")
+  merged.df <- rbind(tdat, sdat)
+  merged.df <- subset(merged.df, select = c(Student.code,
+                                            Teacher.code,
+                                            Subject,
+                                            Source,
+                                            Category,
+                                            Score))
+  return(merged.df)
+}
 
 xlsMerge <- function(dir = getwd(), sheet.index = 1, colRange = 1:11) {
   ### Merges all xls, and xlsx files in the folder into one data frame
@@ -20,3 +45,13 @@ xlsMerge <- function(dir = getwd(), sheet.index = 1, colRange = 1:11) {
   return(df)
 }
 
+teacher.dat <- xlsMerge(teacher.dat.folder)
+teacher.dat <- teacher.dat[complete.cases(teacher.dat),]
+write.csv(teacher.dat, teacher.dat.file, row.names = FALSE)
+
+effort.data <- effortMergeData(student.dat, teacher.dat)
+write.csv(effort.data, "effort_report_data.csv", row.names = FALSE)
+
+student.info <- data.frame(Student.code = student.dat$Student.code, Email = student.dat$Email,
+                            Student.name = paste(student.dat$First.name, student.dat$Surname))
+student.info <- unique.data.frame(student.info)
--- effort_report_script.R
+++ effort_report_script.R
@@ -6,35 +6,48 @@
 # The files required are:
 #    1. effort.functions.R
 #    2. effort_report_script.R
-#    3. effort_report_markdown.Rmd
-#    4. effort_report_data.csv
+#    3. student_effort_report_markdown.Rmd
+#    4. teacher_effort_report_markdown.Rmd 
+#    5. school_effort_report_markdown.Rmd
+#    6. effort_report_data.csv
+#    7. student.names.csv   -- A csv file of student number to name conversions, fields are "Student.code" and "Student.name"
 # The files need to be in the same folder. 
-
-# The data needs to have the following fields:
-#    1. Student.ID
-#    2. Teacher.ID
-#    3. Subject
-#    4. Category (Diligence, Engagement, Behaviour)
-#    5. Score (1 to 5, 1 being unsatisfactory, 5 being Outstanding)
 
 library(knitr)
 library(rmarkdown)
 library(markdown)
 
-# Creates a directory called 'reports' if it does not already exist
-if (!dir.exists("reports")) {dir.create("reports")}
-
 # Reading in effort data file called 'effort_report_data.csv"
 # Data should have the following fields:
-#   Student.ID
+#   Student.code
+#   Teacher.code
 #   Category 
 #   Subject
 #   Score
 #   Source
 effort.data <- read.csv("effort_report_data.csv")
-student.numbers <- unique(effort.data$Student.ID)
+student.numbers <- unique(effort.data$Student.code)
+teacher.codes <- unique(effort.data$Teacher.code)
 
+# Creates a directory called 'reports' if it does not already exist
+if (!dir.exists("reports")) {dir.create("reports")}
+
+# Creating student reports
 for (ID in student.numbers) {
-  # main loop
-  
+  s.name <- student.info[student.info$Student.code == ID,]$Student.name
+  rmarkdown::render('student_effort_report_markdown.Rmd',
+                    output_file = paste0("Student_Effort_Report_", s.name , "_", Sys.Date(), ".html" ),
+                    output_dir = "reports")
 }
+
+# Creating teacher reports
+############## commented out until markdown ready #########
+for (tcode in teacher.codes) {
+  rmarkdown::render('teacher_effort_report_markdown.Rmd',
+                    output_file = paste0("Class_Effort_Report_", tcode, "_", Sys.Date(), ".html" ),
+                    output_dir = "reports")
+}
+
+# Creating school reports
+
+# To be implemented
